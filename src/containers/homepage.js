7b5215c221b504915d7282ac18c1533161bc9810
Gmes23 <gmesda23@gmail.com>
2017-04-27 19:10:45 -0400

    finish structure of app for homepage

--- src/containers/homepage.js
+++ src/containers/homepage.js
@@ -3,30 +3,62 @@ import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
 import { Link } from 'react-router';
 
-
-
 export default class HomePage extends Component {
 
     render() {
         return (
             <div>
-                <ul>
-                <li>
-                    <Link to="/"> Home </Link>
-                </li>
-                 <li>
-                    <Link to="/blog"> Blog </Link>
-                </li>
-                 <li>
-                    <Link to="/about"> About </Link>
-                </li>
-                 <li>
-                    <Link to="/contact"> Contact </Link>
-                </li>
-                </ul>
-                <h1> Gerardo Mestanza </h1>
-                <h2> Software Engineer </h2>
-                
+                <div className="navbar-gm">
+                    <div className="logo-gm-container">
+                        <h1 className="logo-gm no-margin">GM</h1>
+                    </div>
+                    <div className="logo-description">
+                        <h1 className="no-margin gm-decription-header"> Gerardo Mestanza - Portfolio </h1>
+                    </div>
+                </div>
+                <div className="right-container-mainpage">
+                    <div className="right-container-top-section">
+                        <p> blog </p>
+                    </div>
+                    <div className="right-container-bottom-section">
+                        <p>instagram or portfolio </p>
+                    </div>
+                </div>
+                <div className="billboard-mainpage">
+                    <span className="span-line-small" />
+                    <div className="billboard-display-events">
+                        <h1 className="billboard-text"> Gerardo Mestanza
+                                    <br />
+                            Software Engineer
+                                    <br />
+                            Creative Design
+                                </h1>
+                    </div>
+                    <div className="billboard-link-to">
+                        <span className="span-line-on-text">
+                        </span>
+                            <Link to='/about'> View Projects </Link>
+                    </div>
+
+                </div>
+                <div className="footer-mainpage">
+                    <div className="email-container">
+                        <p className="email-gm"> Gerardomestanza23@gmail.com </p>
+                    </div>
+                    <div className="social-links">
+                        <ul>
+                            <li>
+                                <i className="fa fa-linkedin fa-lg social-icons" aria-hidden="true"></i>
+                            </li>
+                            <li>
+                                <i className="fa fa-github fa-lg social-icons" aria-hidden="true"></i>
+                            </li>
+                            <li>
+                                <i className="fa fa-instagram fa-lg social-icons" aria-hidden="true"></i>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         )
     }
