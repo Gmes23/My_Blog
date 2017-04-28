7d5e2eaa80f452d88a11563c66bb85e20b6a5385
Gmes23 <gmesda23@gmail.com>
2017-04-27 22:45:04 -0400

    finish homepage layout

--- src/containers/homepage.js
+++ src/containers/homepage.js
@@ -13,15 +13,30 @@ export default class HomePage extends Component {
                         <h1 className="logo-gm no-margin">GM</h1>
                     </div>
                     <div className="logo-description">
-                        <h1 className="no-margin gm-decription-header"> Gerardo Mestanza - Portfolio </h1>
+                        <h1 className="no-margin gm-description-header"> Gerardo Mestanza - Portfolio </h1>
                     </div>
                 </div>
                 <div className="right-container-mainpage">
                     <div className="right-container-top-section">
-                        <p> blog </p>
+                        <h1 className="no-margin title-section-right-container"> Blog </h1>
+                        <div className="top-section-actions">
+                            <div className="image-holder">
+                                <img className="blog-pictures" src="./img/placeholder.jpg" />
+                            </div>
+                            <div className="blog-name">
+                                <p> Basic_Mathematical_Operations.js</p>
+                                <p> Javascript </p>
+                            </div>
+                        </div>
                     </div>
                     <div className="right-container-bottom-section">
-                        <p>instagram or portfolio </p>
+                        <div className="bottom-section-actions">
+                            <h1 className="center-text bottom-right-container-title"> Works </h1>
+                                 <div className="image-holder">
+                                <img className="blog-pictures" src="./img/placeholder.jpg" />
+                            </div>
+                          
+                        </div>
                     </div>
                 </div>
                 <div className="billboard-mainpage">
@@ -37,13 +52,13 @@ export default class HomePage extends Component {
                     <div className="billboard-link-to">
                         <span className="span-line-on-text">
                         </span>
-                            <Link to='/about'> View Projects </Link>
+                        <Link to='/about'> View Projects </Link>
                     </div>
 
                 </div>
                 <div className="footer-mainpage">
                     <div className="email-container">
-                        <p className="email-gm"> Gerardomestanza23@gmail.com </p>
+                        <p className="email-gm"> GERARDOMESTANZA23@GMAIL.COM </p>
                     </div>
                     <div className="social-links">
                         <ul>
